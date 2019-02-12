5533380299598350366546f2833ec5d1b93bbb8d
Otasag <lnq.fpt@gmail.com>
2019-02-12 16:45:19 +0700

    update code:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email : "",
+      password : "",
+      description : "",
+      gender : 0,
+      lang : "FR",
+      active : true
+    };
+
+    this.onhandleChange =  this.onhandleChange.bind(this);
+    this.onhandleSubmit = this.onhandleSubmit.bind(this);
+
+  }
+  onhandleChange(event)
+  {
+    //console.log(event.target.name);
+    //console.log(event.target.value);
+    var name = event.target.name;
+    var value = event.target.type === "checkbox" ? event.target.checked : event.target.value;
+    this.setState({
+        [name] : value
+    });
+  }
+
+  onhandleSubmit(event)
+  {
+    event.preventDefault();
+    console.log(this.state);
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <form onSubmit={this.onhandleSubmit}>
+          <div className="col-md-6">
+            <div className="form-group">
+              <label> Email :  </label>
+              <input type="text" name="email" onChange={this.onhandleChange } className="form-control" aria-describedby="helpId" />
+              <small id="helpId" className="text-muted">Help text</small>
+            </div>
+            <div className="form-group">
+              <label> Password </label>
+              <input type="password" name="password" onChange={this.onhandleChange } className="form-control" aria-describedby="helpId" />
+              <small id="helpId" className="text-muted">Help text</small>
+            </div>
+            <div className="form-group">
+              <label> Description :  </label>
+              <textarea type="text" name="description" onChange={this.onhandleChange } className="form-control" aria-describedby="helpId" />
+              <small id="helpId" className="text-muted">Help text</small>
+            </div>
+
+            <div className="form-group">
+              <label> Level :  </label>
+              <select className="form-control" name="gender" onChange={this.onhandleChange } value={this.state.gender}>
+                <option value={0}>Male</option>
+                <option value={1}>Famele</option>
+              </select>
+              <small id="helpId" className="text-muted">Help text</small>
+            </div>
+            
+            <div className="form-group">
+              <label> Languages : </label> <br/>
+              <label> English  <input type="radio"
+                                        name="lang" 
+                                        onChange={this.onhandleChange } 
+                                        value="EN" 
+                                        checked={this.state.lang === 'EN'}  />  </label>    
+              <br/>        
+              <label> France  <input type="radio" 
+                                        name="lang" 
+                                        onChange={this.onhandleChange } 
+                                        value="FR"
+                                        checked={this.state.lang === 'FR'} /> </label> 
+            </div>
+
+
+            <div className="form-group">
+              <label> Status  <input type="checkbox"
+                                        name="active" 
+                                        onChange={this.onhandleChange } 
+                                        value={this.state.active}
+                                        checked={this.state.active === true}
+                                        
+                                        />  </label>    
+              <br/>        
+              
+            </div>
+
+
+            <div className="form-group">
+              <input type="submit" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
